89d6ec681c25d6e5ab50777e850872db2603fec5
estebang625 <estebang7511@gmail.com>
2020-10-20 23:09:17 -0700

    frontend format

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./App.css";
+import { Route, Link, Switch } from "react-router-dom";
+import Display from "./Display";
+import Form from "./Form";
+function App() {
+    
+}
